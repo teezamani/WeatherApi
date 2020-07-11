6c3b3f945544f14716163295b1eab4c4725b099d
Adepoju <teezamani@gmail.com>
2020-07-11 09:40:15 +0100

    Database access to the application

---
+++ Model/AppDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Model
+{
+    //This base class connects application to Database
+    public class AppDBContext : DbContext
+    {
+
+        //Contructor Injector
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
